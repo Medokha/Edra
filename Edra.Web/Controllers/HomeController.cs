666ddd4009d624f401dbe34b75f0790209c04937
Ragab Mohamed <itblocks@DESKTOP-RKI6K5R>
2026-01-09 11:52:31 +0200

    initial Commit

--- Edra.Web/Controllers/HomeController.cs
+++ Edra.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using Edra.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Edra.Web.Controllers
@@ -23,10 +22,6 @@ namespace Edra.Web.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+  
     }
 }
